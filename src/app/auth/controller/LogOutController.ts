4b6ca2472f4a103b14064e8f801a07729b83411d
Enrique Marin <enrique_marin_h@hotmail.com>
2024-01-18 17:25:07 -0700

    fix: package lock add

--- src/app/auth/controller/LogOutController.ts
+++ src/app/auth/controller/LogOutController.ts
@@ -1,10 +1,14 @@
 import { Request, Response } from 'express'
 import { logger } from '@shared/log/logger'
 import HttpStatusCode from '@shared/enums/httpStatusCode'
+import { decodeToken } from '@/plugins/jwt/decodeToken'
 
 export default class LogOutController {
   async execute(req: Request, res: Response) {
-    logger({ HttpType: 'POST', route: '/auth/logout', useremail: req.body.email, success: true })
-    res.status(HttpStatusCode.OK).json({ message: 'Logout successfully' })
+    const token = req.headers['authorization']
+    const decodedToken = decodeToken(token)
+
+    logger({ HttpType: 'POST', route: '/auth/logout', useremail: decodedToken.email, success: true })
+    res.status(HttpStatusCode.OK).json({ data: 'Logout successfully' })
   }
 }
--- src/app/auth/router.ts
+++ src/app/auth/router.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import SignInController from './controller/SignInController'
 import RecoverPassController from './controller/RecoverPassController'
 import LogOutController from './controller/LogOutController'
+import VerifyAuthMiddleware from '../middleware/VerifyAuthMiddleware'
 
 export const AuthRoutes = () => {
   const router = express.Router()
@@ -9,10 +10,12 @@ export const AuthRoutes = () => {
   const signInController = new SignInController()
   const recoverPassController = new RecoverPassController()
   const logOutController = new LogOutController()
+  // Middlewares
+  const verifyAuthMiddleware = new VerifyAuthMiddleware()
   // Routes
   router.post('/token', signInController.execute)
   router.get('/recover/:email', recoverPassController.execute)
-  router.post('/logout', logOutController.execute)
+  router.post('/logout', verifyAuthMiddleware.execute, logOutController.execute)
 
 
   return router
